50dae5131ef1827d37a8b663ee761379b5689146
Arindam Singh <98768062+Arindam2407@users.noreply.github.com>
2023-03-04 13:57:05 +0000

    Update transactions.spec.ts

--- test/hub/interactions/transactions.spec.ts
+++ test/hub/interactions/transactions.spec.ts
@@ -1,28 +1,20 @@
 import '@nomiclabs/hardhat-ethers';
 import { expect } from 'chai';
-import { MAX_UINT256, ZERO_ADDRESS } from '../../helpers/constants';
+import { ZERO_ADDRESS } from '../../helpers/constants';
 import { ERRORS } from '../../helpers/errors';
 import { ethers, network } from 'hardhat';
 import {
-  postReturningTokenId,
-} from '../../helpers/utils';
-import {
   FIRST_PROFILE_ID,
-  accounts,
-  governance,
   rejig,
+  rejigERC20Address,
   makeSuiteCleanRoom,
-  mockModuleData,
-  mockReferenceModule,
   MOCK_FOLLOW_NFT_URI,
   MOCK_FOLLOW_NFT_URI_2,
   MOCK_PROFILE_HANDLE,
   MOCK_PROFILE_HANDLE_2,
   MOCK_PROFILE_URI,
   MOCK_URI,
-  testWallet,
   userAddress,
-  user,
   userTwo,
   userThree,
   userFour,
@@ -32,13 +24,9 @@ import {
   userEight,
   userNine,
   userTen,
-  userEleven,
-  userTwelve,
-  userThirteen,
-  userFourteen,
-  userFifteen,
-  abiCoder,
   userTwoAddress,
+  MOCK_TRANSACTION_NFT_URI,
+  MOCK_TRANSACTION_NFT_URI_2,
 } from '../../__setup.spec';
 
 let ac;
@@ -54,6 +42,8 @@ makeSuiteCleanRoom('Transactions', function () {
           followModule: ZERO_ADDRESS,
           followModuleInitData: [],
           followNFTURI: MOCK_FOLLOW_NFT_URI,
+          transactionModule: ZERO_ADDRESS,
+          transactionNFTURI: MOCK_TRANSACTION_NFT_URI
         })
       ).to.not.be.reverted;
       await expect(
@@ -64,11 +54,13 @@ makeSuiteCleanRoom('Transactions', function () {
           followModule: ZERO_ADDRESS,
           followModuleInitData: [],
           followNFTURI: MOCK_FOLLOW_NFT_URI_2,
+          transactionModule: ZERO_ADDRESS,
+          transactionNFTURI: MOCK_TRANSACTION_NFT_URI_2
         })
       ).to.not.be.reverted;
     });
     context('Buy NFT Scenarios', function () {
-        it('Approved users should be able to buy NFTs', async function () {
+        beforeEach(async function () {
           const list = [userTwo,userThree,userFour,userFive,userSix,userSeven,userEight,userNine,userTen];
   
           for(let i = 1; i < list.length; i++){
@@ -81,12 +73,23 @@ makeSuiteCleanRoom('Transactions', function () {
               contentURI: MOCK_URI,
               referenceModule: ZERO_ADDRESS,
               referenceModuleInitData: [],
-          }, ethers.utils.parseEther('0.05'), ethers.utils.parseEther('0.0000000001'),[])).to.not.be.reverted;
+          }, ethers.utils.parseEther('0.05'), ethers.utils.parseEther('0.0000000001'),
+          { token1: rejigERC20Address,
+            noTokens1: 1000000,
+            token2: ZERO_ADDRESS,
+            noTokens2: 0,
+            token3: ZERO_ADDRESS,
+            noTokens3: 0
+          })).to.not.be.reverted;
 
           await network.provider.send("evm_increaseTime", [2*60*60*24]);
           await network.provider.request({ method: "evm_mine", params: [] });
-
-          await expect(rejig.connect(userTwo).buyNFT(userAddress,0,0, {value: ethers.utils.parseEther('0.01')})).to.not.be.reverted;
+        });
+        it('Approved users should not be able to buy NFTs without sending enough ETH', async function () {
+          await expect(rejig.connect(userTwo).buyNFT(userAddress,0,2)).to.be.revertedWith(ERRORS.NOT_ENOUGH_ETH);
+        });
+        it('Approved users should be able to buy NFTs with sending enough ETH and payments should be settled', async function () {
+          await expect(rejig.connect(userTwo).buyNFT(userAddress,0,2, {value: ethers.utils.parseEther('1')})).to.not.be.reverted;
         });
     });
 });
