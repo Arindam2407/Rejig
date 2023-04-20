bf2fb531da9544e22b855cdf16ee8e0b0287f02e
Arindam Singh <98768062+Arindam2407@users.noreply.github.com>
2023-04-20 22:31:03 +0100

    Update deploy-localhost.ts

--- tasks/deploy-localhost.ts
+++ tasks/deploy-localhost.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/hardhat-ethers";
 import { task } from "hardhat/config";
+import {frontEndContractsFile, frontEndAbiFile} from "../helper-hardhat-config"
 import fs from "fs";
 
 const TREASURY_FEE_BPS = 50;
@@ -7,7 +8,7 @@ const REJIG_NFT_NAME = 'Rejig App Profiles';
 const REJIG_NFT_SYMBOL = 'RAP';
 const BASE_FEE = "250000000000000000"
 const GAS_PRICE_LINK = 1e9
-const GAS_LANE =  "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15";
+const GAS_LANE =  "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c";
 const CALLBACK_GAS_LIMIT = "500000"
 
 import { deployContract, waitForTx } from "./helpers/utils";
@@ -27,9 +28,10 @@ import { ModuleGlobals__factory,
         FollowerOnlyReferenceModule__factory,
         UIDataProvider__factory,
         ProfileCreationProxy__factory,
-        VRFCoordinatorV2Mock__factory,} from "../typechain-types";
+        VRFCoordinatorV2Mock__factory,
+        TransactionNFT__factory,} from "../typechain-types";
 
-task('full-deploy', 'deploys the entire Rejig App').setAction(async ({}, hre ) => {
+task('deploy-localhost', 'deploys the entire Rejig App').setAction(async ({}, hre ) => {
     const ethers = hre.ethers;
     const chainId = hre.network.config.chainId
 
@@ -78,19 +80,21 @@ task('full-deploy', 'deploys the entire Rejig App').setAction(async ({}, hre ) =
             new VRFCoordinatorV2Mock__factory(deployer).deploy(BASE_FEE,GAS_PRICE_LINK,{nonce: deployerNonce++})
         );
 
-        const followNFTNonce = ethers.utils.hexlify(deployerNonce + 1);
-        const hubProxyNonce = ethers.utils.hexlify(deployerNonce + 2);
+        const hubLibs = {
+          'contracts/libraries/InteractionLogic.sol:InteractionLogic': interactionLogic.address,
+          'contracts/libraries/PostNFTTokenURILogic.sol:PostNFTTokenURILogic':
+          postNFTTokenURILogic.address,
+          'contracts/libraries/ProfileTokenURILogic.sol:ProfileTokenURILogic':
+            profileTokenURILogic.address,
+          'contracts/libraries/PublishingLogic.sol:PublishingLogic': publishingLogic.address};
+
+        const followNFTNonce = ethers.utils.hexlify(deployerNonce + 2);
+        const transactionNFTNonce = ethers.utils.hexlify(deployerNonce + 3)
+        const hubProxyNonce = ethers.utils.hexlify(deployerNonce + 5);
 
         const followNFTImplAddress = '0x' + ethers.utils.keccak256(ethers.utils.RLP.encode([deployer.address, followNFTNonce])).substring(26);
+        const transactionNFTImplAddress = '0x' + ethers.utils.keccak256(ethers.utils.RLP.encode([deployer.address, transactionNFTNonce])).substring(26);
         const hubProxyAddress = '0x' + ethers.utils.keccak256(ethers.utils.RLP.encode([deployer.address, hubProxyNonce])).substring(26);
-
-        const hubLibs = {
-            'contracts/libraries/InteractionLogic.sol:InteractionLogic': interactionLogic.address,
-            'contracts/libraries/PostNFTTokenURILogic.sol:PostNFTTokenURILogic':
-            postNFTTokenURILogic.address,
-            'contracts/libraries/ProfileTokenURILogic.sol:ProfileTokenURILogic':
-              profileTokenURILogic.address,
-            'contracts/libraries/PublishingLogic.sol:PublishingLogic': publishingLogic.address};
           
         let SUB_ID;
         const txResponse = await vRFCoordinatorV2Mock.connect(deployer).createSubscription({ nonce: deployerNonce++ })
@@ -103,17 +107,21 @@ task('full-deploy', 'deploys the entire Rejig App').setAction(async ({}, hre ) =
               SUB_ID,
               GAS_LANE,
               CALLBACK_GAS_LIMIT, 
-              followNFTImplAddress, {nonce: deployerNonce++})
+              followNFTImplAddress, transactionNFTImplAddress, {nonce: deployerNonce++})
+        );
+
+        console.log('\n\t-- Deploying Follow NFT and Transaction NFT --');
+        const followNFT = await deployContract(
+            new FollowNFT__factory(deployer).deploy(hubProxyAddress, {nonce: deployerNonce++})
+        );
+
+        const transactionNFT = await deployContract(
+          new TransactionNFT__factory(deployer).deploy(hubProxyAddress, { nonce: deployerNonce++ })
         );
 
         const consumertxResponse = await vRFCoordinatorV2Mock.connect(deployer).addConsumer(SUB_ID,rejigImpl.address,
           { nonce: deployerNonce++ })
         await consumertxResponse.wait()
-
-        console.log('\n\t-- Deploying Follow NFT --');
-        const followNFT = await deployContract(
-            new FollowNFT__factory(deployer).deploy(hubProxyAddress, {nonce: deployerNonce++})
-        );
 
         let data = rejigImpl.interface.encodeFunctionData('initialize', [
             REJIG_NFT_NAME,
@@ -186,6 +194,8 @@ task('full-deploy', 'deploys the entire Rejig App').setAction(async ({}, hre ) =
     new ProfileCreationProxy__factory(deployer).deploy(profileCreatorAddress, rejig.address, {
       nonce: deployerNonce++,
     }));
+  
+  
 
   // Whitelist
   let governanceNonce = await ethers.provider.getTransactionCount(governance.address);
@@ -195,18 +205,29 @@ task('full-deploy', 'deploys the entire Rejig App').setAction(async ({}, hre ) =
       .connect(governance)
       .whitelistCurrency(currency.address, true, { nonce: governanceNonce++ })
   );
+
+  //Unpause
+  console.log('\n\t-- Unpausing');
+  await waitForTx(
+    rejig.connect(governance).setState(0, {nonce : governanceNonce++})
+  );
+
   console.log("----------------------------------------------------------")
   console.log("You are deploying to a local network, you'll need a local network running to interact")
   console.log("Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!")
   console.log("----------------------------------------------------------")
 
-  // Save and log the addresses
+  console.log("Writing to front end...")
+        
+        // Save and log the addresses
   const addrs = {
     'rejig proxy': rejig.address,
-    'rejig impl:': rejigImpl.address,
+    'rejig impl': rejigImpl.address,
+    'chainlink coordinator' : vRFCoordinatorV2Mock.address,
     'publishing logic lib': publishingLogic.address,
     'interaction logic lib': interactionLogic.address,
     'follow NFT impl': followNFTImplAddress,
+    'transaction NFT impl': transactionNFTImplAddress,
     'currency' : currency.address,
     'rejig periphery': rejigPeriphery.address,
     'module globals': moduleGlobals.address,
@@ -215,12 +236,34 @@ task('full-deploy', 'deploys the entire Rejig App').setAction(async ({}, hre ) =
     'revert follow module': revertFollowModule.address,
     'follower only reference module': followerOnlyReferenceModule.address,
     'UI data provider': uiDataProvider.address,
-    'Profile creation proxy': profileCreationProxy.address,
+    'Profile creation proxy': profileCreationProxy.address
   };
   const json = JSON.stringify(addrs, null, 2);
   console.log(json);
     
-  fs.writeFileSync('addresses.json', json, 'utf-8');
-  }
+  fs.writeFileSync(frontEndContractsFile, json, 'utf-8');
+        
+  const abis = {
+    'rejig proxy': Rejig__factory.abi,
+    'rejig impl': Rejig__factory.abi,
+    'publishing logic lib': PublishingLogic__factory.abi,
+    'interaction logic lib': InteractionLogic__factory.abi,
+    'follow NFT impl': FollowNFT__factory.abi,
+    'transaction NFT impl': TransactionNFT__factory.abi,
+    'currency' : Currency__factory.abi,
+    'rejig periphery': RejigPeriphery__factory.abi,
+    'module globals': ModuleGlobals__factory.abi,
+    'fee follow module': FeeFollowModule__factory.abi,
+    'profile follow module': ProfileFollowModule__factory.abi,
+    'revert follow module': RevertFollowModule__factory.abi,
+    'follower only reference module': FollowerOnlyReferenceModule__factory.abi,
+    'UI data provider': UIDataProvider__factory.abi,
+    'Profile creation proxy': ProfileCreationProxy__factory.abi,
+  };
+  const jsonabi = JSON.stringify(abis, null, 2);
+  fs.writeFileSync(frontEndAbiFile, jsonabi, 'utf-8');
+
+  console.log("Front end written!")
+}
 
 });
